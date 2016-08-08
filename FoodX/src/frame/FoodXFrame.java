aa66eb13a129820b181973eca88c0a6b2a6e1d01
alinasun <alinasun@usc.edu>
2016-08-07 18:55:25 -0700

    user funct

--- FoodX/src/frame/FoodXFrame.java
+++ FoodX/src/frame/FoodXFrame.java
@@ -12,6 +12,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -69,7 +72,8 @@ public class FoodXFrame extends JFrame {
 		JLabel zipCodeLabel = new JLabel("Please Enter a Zip Code ... ");
 		zipCodeLabel.setOpaque(false);
 		zipCodeEnter = new JTextField(10);
-		//if(userType == "user") zipCodeEnter.setText(cd.getLastEntry());
+		if(userType == "user") zipCodeEnter.setText(cd.getLastEntry());
+		//if(userType == "user") zipCodeEnter.setText("12345");
 		JButton search = new JButton("Search");
 
 		un = Box.createHorizontalBox();
@@ -107,6 +111,22 @@ public class FoodXFrame extends JFrame {
 		*/
 		search.addActionListener(new LoadRestaurants());
 		zipCodeEnter.addActionListener(new LoadRestaurants());
+		
+		addWindowListener(new WindowAdapter(){
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				cd.addLastEntry(zipCodeEnter.getText());
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				cd.addLastEntry(zipCodeEnter.getText());
+			}
+			
+		});
 	}
 	
 	class LoadRestaurants implements ActionListener{
